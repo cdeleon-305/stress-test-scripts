e6f8336559dc7f4652aa538a6b43fa6afb28902a
Carlos Deleon <carlosdeleon175@gmail.com>
2021-03-20 13:38:29 -0400

    Initial commit for stress test

---
+++ main.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from locust import HttpUser, TaskSet, task, web, between
+from locust import events
+
+
+class MyTaskSet(TaskSet):
+    @task(2)
+    def index(l):
+        l.client.get("/")
+
+    @task(1)
+    def stats(l):
+        l.client.get("/stats/requests")
+
+
+class WebsiteUser(HttpUser):
+    host = "http://127.0.0.1:8089"
+    wait_time = between(2, 5)
+    tasks = [MyTaskSet]
+
+
+stats = {"content-length": 0}
+
+
+@events.init.add_listener
+def locust_init(environment, **kwargs):
+    if environment.web_ui:
+        # this code is only run on the master node (the web_ui instance doesn't exist on workers)
+        @environment.web_ui.app.route("/content-length")
+        def total_content_length():
+            """
+            Add a route to the Locust web app, where we can see the total content-length
+            """
+            return "Total content-length received: %i" % stats["content-length"]
+
+
+@events.request_success.add_listener
+def on_request_success(request_type, name, response_time, response_length):
+    """
+    Event handler that get triggered on every successful request
+    """
+    stats["content-length"] += response_length
+
+
+@events.report_to_master.add_listener
+def on_report_to_master(client_id, data):
+    """
+    This event is triggered on the worker instances every time a stats report is
+    to be sent to the locust master. It will allow us to add our extra content-length
+    data to the dict that is being sent, and then we clear the local stats in the worker.
+    """
+    data["content-length"] = stats["content-length"]
+    stats["content-length"] = 0
+
+
+@events.worker_report.add_listener
+def on_worker_report(client_id, data):
+    """
+    This event is triggered on the master instance when a new stats report arrives
+    from a worker. Here we just add the content-length to the master's aggregated
+    stats dict.
+    """
+    stats["content-length"] += data["content-length"]
